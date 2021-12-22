800e4f2031c4fbd071016ca654b7bb76c80ae550
Harvey <96285184+MrHarveyMarshall@users.noreply.github.com>
2021-12-22 21:00:33 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+input.onSound(DetectedSound.Loud, function () {
+    LightsOn = !(LightsOn)
+    if (LightsOn) {
+        basic.showLeds(`
+            . # # # .
+            . # # # .
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+    } else {
+        basic.clearScreen()
+    }
+})
+let LightsOn = false
+input.setSoundThreshold(SoundThreshold.Loud, 128)
+basic.forever(function () {
+	
+})
